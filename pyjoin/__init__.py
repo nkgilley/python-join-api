ceb384eb21e2b103fc0c355447252fedd7f7a185
Nolan Gilley <nkgilley@gmail.com>
2016-06-17 10:50:03 -0400

    add more functions
    check auth

--- pyjoin/__init__.py
+++ pyjoin/__init__.py
@@ -5,11 +5,42 @@ SEND_URL = "https://joinjoaomgcd.appspot.com/_ah/api/messaging/v1/sendPush?devic
 LIST_URL = "https://joinjoaomgcd.appspot.com/_ah/api/registration/v1/listDevices?apikey="
 
 def get_devices(api_key):
-    devices_json = requests.get(LIST_URL + api_key).json()['records']
-    return [(d['deviceName'], d['deviceId']) for d in devices_json]
+    response = requests.get(LIST_URL + api_key).json()
+    if response.get('success') and not response.get('userAuthError'):
+        return [(r['deviceName'], r['deviceId']) for r in response['records']]
+    return False
 
-def send_notification(device_id, title, text, api_key=None):
-    if api_key:
-        requests.get(SEND_URL + device_id + "&title=" + title + "&text=" + text + "&apikey=" + api_key)
-    else:
-        requests.get(SEND_URL + device_id + "&title=" + title + "&text=" + text)
+def send_notification(device_id, text, title=None, api_key=None):
+    req_url = SEND_URL + device_id + "&text=" + text
+    if title: req_url += "&title=" + title
+    if api_key: req_url += "&apikey=" + api_key
+    requests.get(req_url)
+
+def ring_device(device_id, api_key=None):
+    req_url = SEND_URL + device_id + "&find=true" if not api_key else SEND_URL + device_id + "&find=true&apikey=" + api_key
+    requests.get(req_url)
+
+def send_url(device_id, url, title=None, text=None, api_key=None):
+    req_url = SEND_URL + device_id + "&url=" + url
+    if title: req_url += "&title=" + title
+    req_url += "&text=" + text if text else "&text="
+    if api_key: req_url += "&apikey=" + api_key
+    requests.get(req_url)
+
+def set_wallpaper(device_id, url, api_key=None):
+    req_url = SEND_URL + device_id + "&wallpaper=" + url
+    if api_key: req_url += "&apikey=" + api_key
+    requests.get(req_url)
+
+def send_file(device_id, url, title=None, text=None, api_key=None):
+    req_url = SEND_URL + device_id + "&file=" + url
+    if title: req_url += "&title=" + title
+    req_url += "&text=" + text if text else "&text="
+    if api_key: req_url += "&apikey=" + api_key
+    requests.get(req_url)
+
+def send_sms(device_id, sms_number, sms_text, api_key=None):
+    req_url = SEND_URL + device_id + "&smsnumber=" + sms_number + "&smstext=" + sms_text
+    if api_key: req_url += "&apikey=" + api_key
+    requests.get(req_url)
+
