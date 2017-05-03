86e801230df2bdfb7478bb9b97a020c64a0d2dcc
Nolan Gilley <nkgilley@gmail.com>
2017-05-03 16:19:37 -0400

    update for pypi

--- setup.py
+++ setup.py
@@ -1,10 +1,44 @@
-from setuptools import setup
+#!/usr/bin/env python
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+license = """
+MIT License
+Copyright (c) 2017 Nolan Gilley
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
 
 setup(name='python-join-api',
       version='0.0.1',
-      description='Python API for viewing Plex activity',
+      description='Python API for interacting with Join by joaoapps.',
       url='https://github.com/nkgilley/python-join-api',
+      download_url='https://github.com/nkgilley/python-join-api/archive/0.0.1.tar.gz',
       author='Nolan Gilley',
+      author_email='nkgilley@gmail.com',
       license='MIT',
       install_requires=['requests>=2.0'],
       packages=['pyjoin'],
