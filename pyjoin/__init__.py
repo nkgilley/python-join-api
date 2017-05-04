e14e6b4f41c3297f3d3cd3258b2c3566ce8cd01e
Nolan Gilley <nkgilley@gmail.com>
2017-05-04 14:15:50 -0400

    use api key for all commands. add vibration. add device_ids and device_names

--- pyjoin/__init__.py
+++ pyjoin/__init__.py
@@ -1,7 +1,7 @@
 """Python API for using Join by joaoapps."""
 import requests
 
-SEND_URL = "https://joinjoaomgcd.appspot.com/_ah/api/messaging/v1/sendPush?deviceId="
+SEND_URL = "https://joinjoaomgcd.appspot.com/_ah/api/messaging/v1/sendPush?apikey="
 LIST_URL = "https://joinjoaomgcd.appspot.com/_ah/api/registration/v1/listDevices?apikey="
 
 def get_devices(api_key):
@@ -10,39 +10,59 @@ def get_devices(api_key):
         return [(r['deviceName'], r['deviceId']) for r in response['records']]
     return False
 
-def send_notification(device_id, text, title=None, icon=None, smallicon=None, api_key=None):
-    req_url = SEND_URL + device_id + "&text=" + text
+def send_notification(api_key, text, device_id=None, device_ids=None, device_names=None, title=None, icon=None, smallicon=None, vibration=None):
+    if device_id is None and device_ids is None and device_names is None: return False
+    req_url = SEND_URL + api_key + "&text=" + text
     if title: req_url += "&title=" + title
     if icon: req_url += "&icon=" + icon
     if smallicon: req_url += "&smallicon=" + smallicon
-    if api_key: req_url += "&apikey=" + api_key
+    if vibration: req_url += "&vibration=" + vibration
+    if device_id: req_url += "&deviceId=" + device_id
+    if device_ids: req_url += "&deviceIds=" + device_ids
+    if device_names: req_url += "&deviceNames=" + device_names
     requests.get(req_url)
 
-def ring_device(device_id, api_key=None):
-    req_url = SEND_URL + device_id + "&find=true" if not api_key else SEND_URL + device_id + "&find=true&apikey=" + api_key
+def ring_device(api_key, device_id=None, device_ids=None, device_names=None):
+    if device_id is None and device_ids is None and device_names is None: return False
+    req_url = SEND_URL + api_key + "&find=true"
+    if device_id: req_url += "&deviceId=" + device_id
+    if device_ids: req_url += "&deviceIds=" + device_ids
+    if device_names: req_url += "&deviceNames=" + device_names
     requests.get(req_url)
 
-def send_url(device_id, url, title=None, text=None, api_key=None):
-    req_url = SEND_URL + device_id + "&url=" + url
+def send_url(api_key, url, device_id=None, device_ids=None, device_names=None, title=None, text=None):
+    if device_id is None and device_ids is None and device_names is None: return False
+    req_url = SEND_URL + api_key + "&url=" + url
     if title: req_url += "&title=" + title
     req_url += "&text=" + text if text else "&text="
-    if api_key: req_url += "&apikey=" + api_key
+    if device_id: req_url += "&deviceId=" + device_id
+    if device_ids: req_url += "&deviceIds=" + device_ids
+    if device_names: req_url += "&deviceNames=" + device_names
     requests.get(req_url)
 
-def set_wallpaper(device_id, url, api_key=None):
-    req_url = SEND_URL + device_id + "&wallpaper=" + url
-    if api_key: req_url += "&apikey=" + api_key
+def set_wallpaper(api_key, url, device_id=None, device_ids=None, device_names=None):
+    if device_id is None and device_ids is None and device_names is None: return False
+    req_url = SEND_URL + api_key + "&wallpaper=" + url
+    if device_id: req_url += "&deviceId=" + device_id
+    if device_ids: req_url += "&deviceIds=" + device_ids
+    if device_names: req_url += "&deviceNames=" + device_names
     requests.get(req_url)
 
-def send_file(device_id, url, title=None, text=None, api_key=None):
-    req_url = SEND_URL + device_id + "&file=" + url
+def send_file(api_key, url, device_id=None, device_ids=None, device_names=None, title=None, text=None):
+    if device_id is None and device_ids is None and device_names is None: return False
+    req_url = SEND_URL + api_key + "&file=" + url
     if title: req_url += "&title=" + title
     req_url += "&text=" + text if text else "&text="
-    if api_key: req_url += "&apikey=" + api_key
+    if device_id: req_url += "&deviceId=" + device_id
+    if device_ids: req_url += "&deviceIds=" + device_ids
+    if device_names: req_url += "&deviceNames=" + device_names
     requests.get(req_url)
 
-def send_sms(device_id, sms_number, sms_text, api_key=None):
-    req_url = SEND_URL + device_id + "&smsnumber=" + sms_number + "&smstext=" + sms_text
-    if api_key: req_url += "&apikey=" + api_key
+def send_sms(api_key, sms_number, sms_text, device_id=None, device_ids=None, device_names=None):
+    if device_id is None and device_ids is None and device_names is None: return False
+    req_url = SEND_URL + api_key + "&smsnumber=" + sms_number + "&smstext=" + sms_text
+    if device_id: req_url += "&deviceId=" + device_id
+    if device_ids: req_url += "&deviceIds=" + device_ids
+    if device_names: req_url += "&deviceNames=" + device_names
     requests.get(req_url)
 
