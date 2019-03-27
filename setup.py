f55654fa19705924612643216c62ceb26772f989
Nolan Gilley <nkgilley@gmail.com>
2019-03-27 12:52:21 -0400

    version 0.0.4

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ SOFTWARE.
 """
 
 setup(name='python-join-api',
-      version='0.0.3',
+      version='0.0.4',
       description='Python API for interacting with Join by joaoapps.',
       url='https://github.com/nkgilley/python-join-api',
       author='Nolan Gilley',
