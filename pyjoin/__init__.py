aee6634a2aabc8f5a2e6d4de478e40cf1e8bd918
Maciej Bieniek <bieniu@users.noreply.github.com>
2019-03-27 17:21:24 +0100

    Add url, sound, tts and language fields

--- pyjoin/__init__.py
+++ pyjoin/__init__.py
@@ -10,7 +10,7 @@ def get_devices(api_key):
         return [(r['deviceName'], r['deviceId']) for r in response['records']]
     return False
 
-def send_notification(api_key, text, device_id=None, device_ids=None, device_names=None, title=None, icon=None, smallicon=None, vibration=None, image=None):
+def send_notification(api_key, text, device_id=None, device_ids=None, device_names=None, title=None, icon=None, smallicon=None, vibration=None, image=None, url=None, tts=None, tts_language=None, sound=None):
     if device_id is None and device_ids is None and device_names is None: return False
     req_url = SEND_URL + api_key + "&text=" + text
     if title: req_url += "&title=" + title
@@ -18,6 +18,10 @@ def send_notification(api_key, text, device_id=None, device_ids=None, device_nam
     if image: req_url += "&image=" + image
     if smallicon: req_url += "&smallicon=" + smallicon
     if vibration: req_url += "&vibration=" + vibration
+    if url: req_url += "&url=" + url
+    if tts: req_url += "&say=" + tts
+    if tts_language: req_url += "&language=" + tts_language
+    if sound: req_url += "&sound=" + sound
     if device_id: req_url += "&deviceId=" + device_id
     if device_ids: req_url += "&deviceIds=" + device_ids
     if device_names: req_url += "&deviceNames=" + device_names
