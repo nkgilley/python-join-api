98af9fb13f82d432bd517a580947d15c92db1cbd
Nolan Gilley <nkgilley@gmail.com>
2020-07-03 02:13:33 +0000

    custom input for actions

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # python-join
 Python API for Join by Joao
+
+## Actions
+Join actions should be provided as a dictionary. Each keys is an action name, and each value is a list of the custome inputs.  Use `None` for no custom input.  For example:
+```
+actions={"Ring":None, "TestInput": ["a","b","c"]}
+```
--- pyjoin/__init__.py
+++ pyjoin/__init__.py
@@ -26,7 +26,14 @@ def send_notification(api_key, text, device_id=None, device_ids=None, device_nam
     if device_id: req_url += "&deviceId=" + device_id
     if device_ids: req_url += "&deviceIds=" + device_ids
     if device_names: req_url += "&deviceNames=" + device_names
-    if actions: req_url += "&actions=" + "|||".join(actions)
+    if actions:
+        action_strings = []
+        for action in actions:
+            action_str = action
+            if actions[action]:
+                action_str += "=:=" + "=:=".join(actions[action])
+            action_strings.append(action_str)
+        req_url += "&actions=" + "|||".join(action_strings)
     requests.get(req_url)
 
 def ring_device(api_key, device_id=None, device_ids=None, device_names=None):
